a25fdae2b73742bb33e32f3e8586fb440ab383dd
Ubuntu <ubuntu@ip-172-31-35-83.us-east-2.compute.internal>
2023-03-05 14:18:46 +0000

    webhook

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello world!");
     System.out.println("Lets learn something new");
+    System.out.println("Start with Jenkins webhook");
   }
 }
